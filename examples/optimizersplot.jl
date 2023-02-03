baed2b473df2eec0b04290f54cb9e51d55132bec
Jorge Gidi <jorgegidi@gmail.com>
2023-02-03 01:52:11 -0300

    improve example

--- examples/optimizersplot.jl
+++ examples/optimizersplot.jl
@@ -1,4 +1,5 @@
 using PaperTools
+using Plots
 
 using OrderedCollections
 
@@ -16,14 +17,23 @@ sp2 = OrderedDict(
     "CSPSA2" => rand(50, 10),
     "CSPSA2 full" => rand(50, 10),
     "CSPSA2 scalar" => rand(50, 10),
-    "CSPSA2 otrawea" => rand(50, 10),
+    "CSPSA2 otro" => rand(50, 10),
 )
 
+sp3 = OrderedDict(
+    "SPSA QN" => rand(50, 10),
+    "CSPSA QN" => rand(50, 10),
+    "CSPSA otro QN " => rand(50, 10),
+    "CSPSA QN scalar" => rand(50, 10),
+    "SPSA QN scalar" => rand(50, 10),
+)
 
 # 'estimator' may be:
 # :mean (showing dispersion as standard deviation)
 # :median (showing dispersion as interquartile range)
-p = optimizersplot(sp1, sp2, sp2,
+p = optimizersplot(sp1, sp2, sp3,
                    estimator = :mean,
                    ylabel = "Cost",
                    )
+
+savefig(p, "example_plot.pdf")
